dff65ec40333df2b77a52250b27bb4c653305f21
Bekzat <bekmukhambetovbk@gmail.com>
2025-01-02 21:46:18 +0100

    feat: -added LoginActivity and SignUpActivity

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+    }
+
+    fun onClickGoSignUp(view : View){
+    val intent = Intent(this,SignUpActivity::class.java )
+        startActivity(intent)
+        finish()
+    }
+}
--- app/src/main/java/com/example/myapplication/SignUpActivity.kt
+++ app/src/main/java/com/example/myapplication/SignUpActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_screen)
+    }
 
+    fun onClickGoLogin(view : View){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
